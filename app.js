cca42c31d1d39482a4e54b9107a830e02b9516d8
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-09-19 18:21:41 +0400

    exam

---
+++ app.js
@@ -0,0 +1,37 @@
+// object-
+// Javascript de icerisinde istediyimiz qeder ve istediyimiz sekilde element
+// ve onun deyerini menimsete bileceyimiz data type larindan biridir,
+//  objectlerin ustunluyu tek ve ayri-ayriliqda verilen
+// deyerleri yazmaq yerine umumilesdirib, birlikde yaza bilirik ve onlara rahatliqla
+//  cata bilirik.
+// misal ucun const object={
+// name:"ali"
+// username:"alion"
+// age:"23"
+// }
+// object icersinde olan elementlerle emeliyyat aparmaq ucun eger misal olaraq
+// object.fullname.name.age yazmaq yerine this.age yaza bilerik.
+// ------------------------------------------------------------------------------------------------
+// function
+// Javascript de funkisyalar icerisinde emeliyyatlar edebileceyimiz, lazim olduqda istenilen yerde
+// cagirabileceyimiz data typelardir, functionlari rahat yazmagin yollarindan biri arrow functiondur
+// ve bu sekilde yazilir const function=()=>{} burada moterize icerisine str yazsaq ve onun
+//  icersinde str a aid emeliyyatlar edebiler ve onu cagirabilerik. functionun constructor,
+// callback kimi novlerini taniyiram, isimize yarayabilecek bir basqa sey ise eger funkisya
+//  yaradib, adini vermesek onun icerisinde 'arguments' olaraq cagira bilerik. ya da bir ad versek
+// meselen const letter(str){
+//burada stri icinde ...str olaraq islede bilerik. }
+// --------------------------------------------------------------------------------------------------------------------
+// heap and stack
+// depolamaq ucun iki temel yaddas adidir.stack da qisa omurluler saxlanilir,funkisya,deyiskenler
+// heap de ise uzun omurlu sekilde yadda saxlanilir, misal olaraq arraylar.
+// javascript de data type larin bir birine menimsedildiyinde ferqli neticeleri olur
+// misal ucun let a="ismail" desek let b="ïsmail" yazsaq, burada let a!=let b,bir bir birilerine
+// beraber deyil, cunku sadece value lari beraberdir,
+// --------------------------------------------------------------------------------------------------------------------
+
+// font case
+// proqramlasdirmada adlandirmaq ucun istifade olunan yazilis qaydalari vardir
+// onlara camel case, pascal case, kebab case, snake case,screaming snake case,
+// camel case - myClassMate, pascal case -MyClassMate, snake case-my_class_mate,
+// kebab case-my-class-mate, screaming snake case-my_class_mate
